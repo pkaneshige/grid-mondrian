cd99af7645041c76fa57af32d0a01b504cc0c7b3
Pace Kaneshige <pwtkaneshige@gmail.com>
2018-10-04 19:44:06 -0400

    Composition II and Composition III

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="wall">
+            <Route exact path="/" component={ Home } />
+            <Route path="/composition-ii" component={ compositionTwo } />
+            <Route path="/composition-iii" component={ compositionThree } />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
 
+const Home = () => (
+  <div>
+    <h2>Grid Mon</h2>
+    <ul style={{textAlign:'left'}}>
+      <li><Link to="/composition-ii">Composition II in Red, Blue, and Yellow (1930)</Link></li>
+      <li><Link to="/composition-iii">Composition III, with Red, Blue, Yellow, and Black (1929)</Link></li>
+    </ul>
+  </div>
+)
+
+const compositionTwo = () => (
+  <div className="square frame">
+    <div className="canvas compositionTwo">
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  </div>
+);
+
+const compositionThree = () => (
+  <div className="square frame">
+    <div className="canvas compositionThree">
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  </div>
+);
 export default App;
